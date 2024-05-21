a6f48e1cb8ba188ed4a43663192d98016826ba5b
RobertoSoto97 <real20@live.com.ar>
2024-05-21 00:00:51 -0300

    Organizando directorios necesarios para empezar a crear el sitio de la liga

---
+++ src/App/Views/not-found.view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require __DIR__ . '/parts/head.view.php'; ?>
+    <link rel="stylesheet" href="/assets/styles/global.css" />
+</head>
+
+<body>
+
+    <!--HEADER-->
+    <?php require __DIR__ . '/parts/header.view.php' ?>
+    <h1 class="titulo-promociones">Pagina no encontrada</h1>
+    
+    <!--FOOTER-->
+    <?php require __DIR__ . '/parts/footer.view.php' ?>
+    
+</body>
+
+</html>
