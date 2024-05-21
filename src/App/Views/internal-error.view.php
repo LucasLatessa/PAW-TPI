efc241be863b24b6d9c40120e54dc8c49223d456
RobertoSoto97 <real20@live.com.ar>
2024-05-21 00:00:51 -0300

    Organizando directorios necesarios para empezar a crear el sitio de la liga

---
+++ src/App/Views/internal-error.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <?php require __DIR__ . '/parts/head.view.php'; ?> <!--reformular si es necesario-->
+        <link rel="stylesheet" href="/assets/styles/global.css" />
+</head>
+<body>
+
+    <!--HEADER-->
+    <?php require __DIR__ . '/parts/header.view.php' ?> <!--reformular si es necesario-->
+    <h1 class="titulo-promociones">Internal error</h1>
+
+    <!--FOOTER-->
+    <?php require __DIR__ . '/parts/footer.view.php' ?>
+
+</body>
+</html>
