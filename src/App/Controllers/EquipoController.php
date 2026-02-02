16c2251597e0486f71365b0ba7a633d71beeb10f
Josue Gatica Odato <josuegaticaodato@gmail.com>
2026-02-02 02:16:02 -0300

    Sitio principal de equipos del torneo

--- src/App/Controllers/EquipoController.php
+++ src/App/Controllers/EquipoController.php
@@ -8,24 +8,41 @@ use Paw\Core\Controlador;
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-class EquipoController extends Controlador{
+class EquipoController extends Controlador
+{
 
-    public ?string $modelName = EquipoCollections::class;
+  public ?string $modelName = EquipoCollections::class;
 
-    public function datosEquipo(){
-        global $request;
-        $title = 'Equipos - LigaCF';
-        $equipo_id = $request->get('id');
-        $equipo = $this->model->getXid($equipo_id);
-        $partidosEquipo= $this->model->getPartidosEquipo($equipo_id);
-        echo $this->twig->render('competencia/equipo.view.twig', [
-            'title' =>  $title,
-            'equipo' => $equipo,
-            'partidosEquipo' => $partidosEquipo,
-        ]);
+  // Ver lista de equipos en la Liga
+  public function equipos()
+  {
+    $hayLogin = $_SESSION['login'];
+    $title = 'Equipos - LigaCF';
+    $equipos = $this->model->getAllEquipos();
 
-    }
+    //var_dump($equipos);
+    echo $this->twig->render('equipos/index.view.twig', [
+      'title' => $title,
+      'equipos' => $equipos, // Pasar la lista de equipos a la vista
+    ]);
+  }
 
+  // Muestra un equipo del torneo
+  public function show()
+  {
+    global $request;
 
+    $title = 'Equipo - LigaCF';
+    $equipo_id = $request->get('id');
+    $equipo = $this->model->getXid($equipo_id);
 
+    //$title = $equipo->getNombrePila() . ' - LigaCF';
+
+    //var_dump($equipo);
+
+    echo $this->twig->render('equipos/show.view.twig', [
+      'title' =>  $title,
+      'equipo' => $equipo
+    ]);
+  }
 }
