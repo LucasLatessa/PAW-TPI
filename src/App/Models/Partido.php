5f77ee07fa4c942583a5ec1ecbeb0c5e335fa143
lucaslatessa <lucaslatessa@gmail.com>
2026-02-17 16:44:05 -0300

    resultados en equipos

--- src/App/Models/Partido.php
+++ src/App/Models/Partido.php
@@ -37,7 +37,7 @@ class Partido extends Model
     public function load(int $id): ?self
     {
         $params = ['id' => $id];
-        $record = current($this->queryBuilder->select($this->table, $params));
+        $record = current($this->queryBuilder->selectLoad($this->table, $params));
 
         if (! $record) {
             return null;
--- src/App/Models/Torneo.php
+++ src/App/Models/Torneo.php
@@ -32,7 +32,7 @@ class Torneo extends Model
     public function load(int $id): ?self
     {
         $params = ['id' => $id];
-        $record = current($this->queryBuilder->select($this->table, $params));
+        $record = current($this->queryBuilder->selectLoad($this->table, $params));
 
         if (! $record) {
             return null;
