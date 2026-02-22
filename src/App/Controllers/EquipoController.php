6ca8956861ac7a4b72b873f7b6aa4c59c62ff9e8
lucaslatessa <lucaslatessa@gmail.com>
2026-02-21 21:38:12 -0300

    arreglo en show equipo no forma parte de torneo

--- src/App/Controllers/EquipoController.php
+++ src/App/Controllers/EquipoController.php
@@ -1,119 +1,124 @@
 <?php
-
 namespace Paw\App\Controllers;
 
-use Paw\App\Models\Equipo;
 use Paw\App\Models\EquipoCollections;
 use Paw\App\Models\EquipoTorneoCollections;
-use Paw\App\Models\PartidoCollections;
 use Paw\App\Models\EstadioCollections;
+use Paw\App\Models\PartidoCollections;
 use Paw\Core\Controlador;
-use Twig\Loader\FilesystemLoader;
-use Twig\Environment;
 
 class EquipoController extends Controlador
 {
 
-  public ?string $modelName = EquipoCollections::class;
+    public ?string $modelName = EquipoCollections::class;
 
-  // Ver lista de equipos en la Liga
-  public function equipos()
-  {
-    $hayLogin = $_SESSION['login'];
-    $title = 'Equipos - LigaCF';
-    $equipos = $this->model->getAllEquipos();
+    // Ver lista de equipos en la Liga
+    public function equipos()
+    {
+        $hayLogin = $_SESSION['login'];
+        $title    = 'Equipos - LigaCF';
+        $equipos  = $this->model->getAllEquipos();
 
-    //var_dump($equipos);
-    echo $this->twig->render('equipos/index.view.twig', [
-      'title' => $title,
-      'equipos' => $equipos, // Pasar la lista de equipos a la vista
-    ]);
-  }
-
-  // Muestra un equipo del torneo
-  public function show()
-{
-    global $request;
-    
-    $equipo_id = $request->get('id');
-    $torneo_id = 1; //Primera
-    $equipo = $this->model->getID($equipo_id);
-
-    // Modelos tabla y partido
-    $partidoModel = new PartidoCollections();
-    $partidoModel->setQueryBuilder($this->getQb());
-    $tablaModel = new EquipoTorneoCollections();
-    $tablaModel->setQueryBuilder($this->getQb());
-    $tablaModel->getLastTorneo($equipo_id);
-   
-    // Buscamos los partidos de este equipo
-    $ultimosPartidos = $partidoModel->getPartidosByEquipo($equipo_id);
-
-    //Posicion actual en primera
-    $posicion = $tablaModel->getPosicion($torneo_id, $equipo_id);
-
-    //Tabla de posiciones en ese torneo
-    $tabla = $tablaModel->getEstadisticas($torneo_id, $equipo_id);
-
-    //Total de equipos en ese torneo
-    $totalEquipos = $tablaModel->getCantidadEquipos($torneo_id);
-
-    //Proximo partido (si existe)
-    $proxPartido = $partidoModel->getProximoPartido($torneo_id,$equipo_id);
-    //var_dump($proxPartido);
-
-    echo $this->twig->render('equipos/show.view.twig', [
-      'title'   => $equipo->getNombre() . ' - LigaCF',
-      'equipo'  => $equipo,
-      'ultimosPartidos' => $ultimosPartidos,
-      'tabla' => $tabla,
-      'posicion' => $posicion,
-      'totalEquipos' => $totalEquipos,
-      'proxPartido' => $proxPartido,
-    ]);
-}
-  public function formCrearEquipo(){
-    $title = 'Crear equipo - LigaCF';
-    echo $this->twig->render('equipos/crearEquipo.view.twig', [
-        'title' =>  $title,
-    ]);
-  }
-  
-  public function crearEquipo()
-  {
-    global $request;
-
-    $nombreEquipo = $request->getRequest('equipo');
-    $nombreInstitucionalEquipo = $request->getRequest('institucional');
-    $fechaCreacion = $request->getRequest('fecha');
-    $nombreEstadio = $request->getRequest('estadio');
-    $descripcion = $request->getRequest('descripcion');
-
-    $nombreArchivo = $this->subirImagen($_FILES, 'escudos');
-    
-
-    if ($nombreArchivo !== false) {
-      $estadioCollections = new EstadioCollections();
-      $estadioCollections->setQueryBuilder($this->getQb());
-      $estadio = $estadioCollections->create($nombreEstadio);
-      $this->model->create($nombreEquipo, $nombreInstitucionalEquipo, $fechaCreacion, $estadio->getId(), $descripcion, $nombreArchivo);
-
-      header('Location: /equipos');
-      exit();
-    } else {
-      $errorMessage = "La imagen excede el tamaño permitido (1MB) o hubo un error en la carga.";
-      $title = "Cargar Equipo - Liga";
-
-      echo $this->twig->render('equipo/crearEquipo.view.twig', [
-        'title' => $title,
-        'errorMessage' => $errorMessage,
-
-        'equipo_ingresado' => $nombreEquipo,
-        'institucional_ingresado' => $nombreInstitucionalEquipo,
-        'fecha_ingresada' => $fechaCreacion,
-        'estadio_ingresado' => $nombreEstadio,
-        'descripcion_ingresada' => $descripcion
-      ]);
+        //var_dump($equipos);
+        echo $this->twig->render('equipos/index.view.twig', [
+            'title'   => $title,
+            'equipos' => $equipos, // Pasar la lista de equipos a la vista
+        ]);
     }
-  }
+
+    // Muestra un equipo del torneo
+    public function show()
+    {
+        global $request;
+
+        $equipo_id = $request->get('id');
+        $equipo    = $this->model->getID($equipo_id);
+
+        // Modelos tabla y partido
+        $partidoModel = new PartidoCollections();
+        $partidoModel->setQueryBuilder($this->getQb());
+        $tablaModel = new EquipoTorneoCollections();
+        $tablaModel->setQueryBuilder($this->getQb());
+        $torneo_id = $tablaModel->getLastTorneo($equipo_id);
+        echo "<pre>";
+        print_r($torneo_id);
+        echo "</pre>";
+        $ultimosPartidos = [];
+        $posicion        = null;
+        $tabla           = null;
+        $totalEquipos    = 0;
+        $proxPartido     = null;
+        if ($torneo_id) {
+
+            // Buscamos los partidos de este equipo
+            $ultimosPartidos = $partidoModel->getPartidosByEquipo($equipo_id);
+
+            //Posicion actual en primera
+            $posicion = $tablaModel->getPosicion($torneo_id, $equipo_id);
+
+            //Tabla de posiciones en ese torneo
+            $tabla = $tablaModel->getEstadisticas($torneo_id, $equipo_id);
+
+            //Total de equipos en ese torneo
+            $totalEquipos = $tablaModel->getCantidadEquipos($torneo_id);
+
+            //Proximo partido (si existe)
+            $proxPartido = $partidoModel->getProximoPartido($torneo_id, $equipo_id);
+
+        }
+
+        echo $this->twig->render('equipos/show.view.twig', [
+            'title'           => $equipo->getNombre() . ' - LigaCF',
+            'equipo'          => $equipo,
+            'ultimosPartidos' => $ultimosPartidos,
+            'tabla'           => $tabla,
+            'posicion'        => $posicion,
+            'totalEquipos'    => $totalEquipos,
+            'proxPartido'     => $proxPartido,
+        ]);
+    }
+    public function formCrearEquipo()
+    {
+        $title = 'Crear equipo - LigaCF';
+        echo $this->twig->render('equipos/crearEquipo.view.twig', [
+            'title' => $title,
+        ]);
+    }
+
+    public function crearEquipo()
+    {
+        global $request;
+
+        $nombreEquipo              = $request->getRequest('equipo');
+        $nombreInstitucionalEquipo = $request->getRequest('institucional');
+        $fechaCreacion             = $request->getRequest('fecha');
+        $nombreEstadio             = $request->getRequest('estadio');
+        $descripcion               = $request->getRequest('descripcion');
+
+        $nombreArchivo = $this->subirImagen($_FILES, 'escudos');
+
+        if ($nombreArchivo !== false) {
+            $estadioCollections = new EstadioCollections();
+            $estadioCollections->setQueryBuilder($this->getQb());
+            $estadio = $estadioCollections->create($nombreEstadio);
+            $this->model->create($nombreEquipo, $nombreInstitucionalEquipo, $fechaCreacion, $estadio->getId(), $descripcion, $nombreArchivo);
+
+            header('Location: /equipos');
+            exit();
+        } else {
+            $errorMessage = "La imagen excede el tamaño permitido (1MB) o hubo un error en la carga.";
+            $title        = "Cargar Equipo - Liga";
+
+            echo $this->twig->render('equipo/crearEquipo.view.twig', [
+                'title'                   => $title,
+                'errorMessage'            => $errorMessage,
+
+                'equipo_ingresado'        => $nombreEquipo,
+                'institucional_ingresado' => $nombreInstitucionalEquipo,
+                'fecha_ingresada'         => $fechaCreacion,
+                'estadio_ingresado'       => $nombreEstadio,
+                'descripcion_ingresada'   => $descripcion,
+            ]);
+        }
+    }
 }
--- src/App/Models/EquipoTorneoCollections.php
+++ src/App/Models/EquipoTorneoCollections.php
@@ -1,303 +1,301 @@
 <?php
-
 namespace Paw\App\Models;
 
-use Paw\Core\Model;
 use Paw\App\Models\Equipo;
-use Paw\App\Models\Partido;
+use Paw\Core\Model;
 
 class EquipoTorneoCollections extends Model
 {
-  public $table = 'equipo_torneo';
+    public $table = 'equipo_torneo';
 
-  public function getAllEquipos($idTorneo)
-  {
-    // Obtener todos los equipos usando el método selectViejo de QueryBuilder
-    $equipos = $this->queryBuilder->selectViejo($this->table, ["torneo_id" => $idTorneo]);
+    public function getAllEquipos($idTorneo)
+    {
+        // Obtener todos los equipos usando el método selectViejo de QueryBuilder
+        $equipos = $this->queryBuilder->selectViejo($this->table, ["torneo_id" => $idTorneo]);
 
-    $equipoCollection = new EquipoCollections();
-    $equipoCollection->setQueryBuilder($this->queryBuilder);
+        $equipoCollection = new EquipoCollections();
+        $equipoCollection->setQueryBuilder($this->queryBuilder);
 
-    // Crear una colección de objetos Equipo
-    $equiposTorneoCollection = [];
-    foreach ($equipos as $equipoData) {
-      $nuevoEquipo = new EquipoTorneo; // Suponiendo que tienes una clase Equipo
-      $nuevoEquipo->set($equipoData);
+        // Crear una colección de objetos Equipo
+        $equiposTorneoCollection = [];
+        foreach ($equipos as $equipoData) {
+            $nuevoEquipo = new EquipoTorneo; // Suponiendo que tienes una clase Equipo
+            $nuevoEquipo->set($equipoData);
 
-      $equipo = $equipoCollection->getId($nuevoEquipo->getEquipo_id());
-      $nuevoEquipo->equipo = $equipo;
-      $equiposTorneoCollection[] = $nuevoEquipo;
-    }
-    //var_dump($equiposTorneoCollection);
-    return $equiposTorneoCollection;
-  }
-
-  public function getTabla($idTorneo)
-  {
-    // Obtener todos los equipos usando el método selectViejo de QueryBuilder
-    $equipos = $this->queryBuilder->selectTabla($this->table, ["torneo_id" => $idTorneo]);
-
-    $equipoCollection = new EquipoCollections();
-    $equipoCollection->setQueryBuilder($this->queryBuilder);
-
-    // Crear una colección de objetos Equipo
-    $equiposTorneoCollection = [];
-    foreach ($equipos as $equipoData) {
-      $nuevoEquipo = new EquipoTorneo; // Suponiendo que tienes una clase Equipo
-      $nuevoEquipo->set($equipoData);
-
-      $equipo = $equipoCollection->getXid($nuevoEquipo->getId_equipo());
-      $nuevoEquipo->equipo = $equipo;
-      $equiposTorneoCollection[] = $nuevoEquipo;
-    }
-    //var_dump($equiposTorneoCollection);
-    return $equiposTorneoCollection;
-  }
-
-  /* Devuelve la ultima tabla creada + el nombre del torneo */
-  public function getUltimaTabla()
-  {
-    // buscar el ultimo torneo creado
-    $torneos = $this->queryBuilder->selectViejo('torneo', [], 'id DESC', 1);
-
-    // si no hay torneos, devolvemos array vacio
-    if (empty($torneos)) {
-      return [];
-    }
-
-    $ultimoTorneo = $torneos[0];
-    $tabla = $this->getTabla($ultimoTorneo['id']);
-
-    return [
-      'nombre_torneo' => $ultimoTorneo['nombre'],
-      'tabla' => $tabla
-    ];
-  }
-  public function create($idEquipo, $idTorneo)
-  {
-    $newEquipoTorneo = new EquipoTorneo;
-
-    $data = [
-      'equipo_id' => $idEquipo,
-      'torneo_id' => $idTorneo
-    ];
-
-    // Asignar el QueryBuilder y establecer los datos del equipo
-    $newEquipoTorneo->setQueryBuilder($this->queryBuilder);
-    $newEquipoTorneo->set($data);
-
-    // Insertar los datos en la base de datos
-    $this->queryBuilder->insert($this->table, $data);
-
-    // Retornar la instancia del nuevo equipo creado
-    return $newEquipoTorneo;
-  }
-  public function getEstadisticas($idTorneo, $equipoId)
-  {
-    $stats = $this->queryBuilder->selectViejo($this->table, ['torneo_id' => $idTorneo, 'equipo_id' => $equipoId]);
-
-    // Asegúrate de que todas las claves necesarias estén presentes
-    $defaultStats = [
-      'partidosJugados' => 0,
-      'partidosGanados' => 0,
-      'partidosEmpatados' => 0,
-      'partidosPerdidos' => 0,
-      'golesAFavor' => 0,
-      'golesContra' => 0,
-      'diferencia' => 0,
-      'puntos' => 0,
-    ];
-
-    // Si no se encuentra el equipo, retorna las estadísticas por defecto
-    if (empty($stats)) {
-      return $defaultStats;
-    } else {
-      return $stats[0];
-    }
-
-    // Combina las estadísticas actuales con las predeterminadas para asegurar que todas las claves estén presentes
-    //return $stats
-  }
-  public function actualizarEstadisticas($localId, $visitanteId, $torneoId, $gl, $gv)
-  {
-    // Local
-    $statsLocal = $this->getEstadisticas($torneoId, $localId);
-    $dataLocal = $this->calcularStats($statsLocal, $gl, $gv);
-
-    $this->queryBuilder->update($this->table, $dataLocal, ['equipo_id' => $localId, "torneo_id" => $torneoId]);
-
-    // Visitante
-    $statsVisitante = $this->getEstadisticas($torneoId, $visitanteId);
-    $dataVisitante = $this->calcularStats($statsVisitante, $gv, $gl);
-
-    $this->queryBuilder->update($this->table, $dataVisitante, ['equipo_id' => $visitanteId, "torneo_id" => $torneoId]);
-  }
-
-  public function revertirEstadisticas($localId, $visitanteId, $torneoId, $gl, $gv)
-  {
-    // LOCAL
-    $statsLocal = $this->getEstadisticas($torneoId, $localId);
-    $dataLocal = $this->calcularStatsReversa($statsLocal, $gl, $gv);
-
-    $this->queryBuilder->update(
-      $this->table,
-      $dataLocal,
-      ['equipo_id' => $localId, "torneo_id" => $torneoId]
-    );
-
-    // VISITANTE
-    $statsVisitante = $this->getEstadisticas($torneoId, $visitanteId);
-    $dataVisitante = $this->calcularStatsReversa($statsVisitante, $gv, $gl);
-
-    $this->queryBuilder->update(
-      $this->table,
-      $dataVisitante,
-      ['equipo_id' => $visitanteId, "torneo_id" => $torneoId]
-    );
-  }
-
-  private function calcularStatsReversa($statsActuales, $gf, $gc)
-  {
-    $pj = $statsActuales['partidos_jugados'] - 1;
-    $pg = $statsActuales['ganados'];
-    $pe = $statsActuales['empatados'];
-    $pp = $statsActuales['perdidos'];
-    $pts = $statsActuales['puntos'];
-
-    if ($gf > $gc) {
-      $pg -= 1;
-      $pts -= 3;
-    } elseif ($gf == $gc) {
-      $pe -= 1;
-      $pts -= 1;
-    } else {
-      $pp -= 1;
-    }
-
-    return [
-      'partidos_jugados'  => $pj,
-      'ganados'  => $pg,
-      'empatados'  => $pe,
-      'perdidos'  => $pp,
-      'goles_favor'  => $statsActuales['goles_favor'] - $gf,
-      'goles_contra'  => $statsActuales['goles_contra'] - $gc,
-      'diferencia_goles'  => ($statsActuales['goles_favor'] - $gf) -
-        ($statsActuales['goles_contra'] - $gc),
-      'puntos' => $pts
-    ];
-  }
-  private function calcularStats($statsActuales, $gf, $gc)
-  {
-    $pj = $statsActuales['partidos_jugados'] + 1;
-    $pg = $statsActuales['ganados'];
-    $pe = $statsActuales['empatados'];
-    $pp = $statsActuales['perdidos'];
-    $pts = $statsActuales['puntos'];
-
-    if ($gf > $gc) {
-      $pg += 1;
-      $pts += 3;
-    } elseif ($gf == $gc) {
-      $pe += 1;
-      $pts += 1;
-    } else {
-      $pp += 1;
-    }
-
-    return [
-      'partidos_jugados'  => $pj,
-      'ganados'  => $pg,
-      'empatados'  => $pe,
-      'perdidos'  => $pp,
-      'goles_favor'  => $statsActuales['goles_favor'] + $gf,
-      'goles_contra'  => $statsActuales['goles_contra'] + $gc,
-      'diferencia_goles'  => ($statsActuales['goles_favor'] + $gf) - ($statsActuales['goles_contra'] + $gc),
-      'puntos' => $pts
-    ];
-  }
-
-  // Obtengo la posicion de un equipo en un torneo
-  public function getPosicion($idTorneo, $idEquipo)
-  {
-    $equipo = $this->queryBuilder->selectViejo(
-      $this->table,
-      [
-        'torneo_id' => $idTorneo,
-        'equipo_id' => $idEquipo
-      ]
-    )[0];
-
-    $tabla = $this->queryBuilder->selectViejo(
-      $this->table,
-      ['torneo_id' => $idTorneo],
-      'puntos DESC, diferencia_goles DESC'
-    );
-
-    $posicion = 1;
-
-    // Con el equipo y la tabla, busco la posicion buscando puntos y DF
-    foreach ($tabla as $fila) {
-
-      if (
-        $fila['puntos'] > $equipo['puntos'] ||
-        (
-          $fila['puntos'] == $equipo['puntos'] &&
-          $fila['diferencia_goles'] > $equipo['diferencia_goles']
-        )
-      ) {
-        $posicion++;
-      }
-    }
-
-    return $posicion;
-  }
-  public function getLastTorneo($idEquipo)
-  {
-    $torneos_disponobles = [ ];
-    $torneos = $this->queryBuilder
-          ->select($this->table)
-          ->where("(equipo_id = :id)")
-          ->setParam('id', $idEquipo)
-          ->execute();
-    
-    foreach ( $torneos as $torneo) {
-      $torneo_id = $torneo['torneo_id'];
-
-      $_torneo = $this->queryBuilder
-          ->select('torneos')
-          ->where("(id = :id AND  estado = 'activo')")
-          ->setParam('id', $torneo_id)
-          ->execute();
-   
-      if (!empty($_torneo)){
-        array_push($torneos_disponobles,$_torneo[0] );
-      }
-    }
-   function primeroCreado(array $torneos)
-{
-    if (empty($torneos)) {
-        return null;
-    }
-
-    $masViejo = $torneos[0];
-
-    foreach ($torneos as $torneo) {
-        if ($torneo['fecha_inicio'] < $masViejo['fecha_inicio']) {
-            $masViejo = $torneo;
+            $equipo                    = $equipoCollection->getId($nuevoEquipo->getEquipo_id());
+            $nuevoEquipo->equipo       = $equipo;
+            $equiposTorneoCollection[] = $nuevoEquipo;
         }
+        //var_dump($equiposTorneoCollection);
+        return $equiposTorneoCollection;
     }
 
-    return $masViejo;
-}
-    
-    $torneos_disponobles = primeroCreado($torneos_disponobles);
+    public function getTabla($idTorneo)
+    {
+        // Obtener todos los equipos usando el método selectViejo de QueryBuilder
+        $equipos = $this->queryBuilder->selectTabla($this->table, ["torneo_id" => $idTorneo]);
 
-    return $torneos_disponobles;
-  }
+        $equipoCollection = new EquipoCollections();
+        $equipoCollection->setQueryBuilder($this->queryBuilder);
 
+        // Crear una colección de objetos Equipo
+        $equiposTorneoCollection = [];
+        foreach ($equipos as $equipoData) {
+            $nuevoEquipo = new EquipoTorneo; // Suponiendo que tienes una clase Equipo
+            $nuevoEquipo->set($equipoData);
 
-  public function getCantidadEquipos($idTorneo)
-  {
-    return $this->queryBuilder->count($this->table, ['torneo_id' => $idTorneo]);
-  }
+            $equipo                    = $equipoCollection->getXid($nuevoEquipo->getId_equipo());
+            $nuevoEquipo->equipo       = $equipo;
+            $equiposTorneoCollection[] = $nuevoEquipo;
+        }
+        //var_dump($equiposTorneoCollection);
+        return $equiposTorneoCollection;
+    }
+
+    /* Devuelve la ultima tabla creada + el nombre del torneo */
+    public function getUltimaTabla()
+    {
+        // buscar el ultimo torneo creado
+        $torneos = $this->queryBuilder->selectViejo('torneo', [], 'id DESC', 1);
+
+        // si no hay torneos, devolvemos array vacio
+        if (empty($torneos)) {
+            return [];
+        }
+
+        $ultimoTorneo = $torneos[0];
+        $tabla        = $this->getTabla($ultimoTorneo['id']);
+
+        return [
+            'nombre_torneo' => $ultimoTorneo['nombre'],
+            'tabla'         => $tabla,
+        ];
+    }
+    public function create($idEquipo, $idTorneo)
+    {
+        $newEquipoTorneo = new EquipoTorneo;
+
+        $data = [
+            'equipo_id' => $idEquipo,
+            'torneo_id' => $idTorneo,
+        ];
+
+        // Asignar el QueryBuilder y establecer los datos del equipo
+        $newEquipoTorneo->setQueryBuilder($this->queryBuilder);
+        $newEquipoTorneo->set($data);
+
+        // Insertar los datos en la base de datos
+        $this->queryBuilder->insert($this->table, $data);
+
+        // Retornar la instancia del nuevo equipo creado
+        return $newEquipoTorneo;
+    }
+    public function getEstadisticas($idTorneo, $equipoId)
+    {
+        $stats = $this->queryBuilder->selectViejo($this->table, ['torneo_id' => $idTorneo, 'equipo_id' => $equipoId]);
+
+        // Asegúrate de que todas las claves necesarias estén presentes
+        $defaultStats = [
+            'partidosJugados'   => 0,
+            'partidosGanados'   => 0,
+            'partidosEmpatados' => 0,
+            'partidosPerdidos'  => 0,
+            'golesAFavor'       => 0,
+            'golesContra'       => 0,
+            'diferencia'        => 0,
+            'puntos'            => 0,
+        ];
+
+        // Si no se encuentra el equipo, retorna las estadísticas por defecto
+        if (empty($stats)) {
+            return $defaultStats;
+        } else {
+            return $stats[0];
+        }
+
+        // Combina las estadísticas actuales con las predeterminadas para asegurar que todas las claves estén presentes
+        //return $stats
+    }
+    public function actualizarEstadisticas($localId, $visitanteId, $torneoId, $gl, $gv)
+    {
+        // Local
+        $statsLocal = $this->getEstadisticas($torneoId, $localId);
+        $dataLocal  = $this->calcularStats($statsLocal, $gl, $gv);
+
+        $this->queryBuilder->update($this->table, $dataLocal, ['equipo_id' => $localId, "torneo_id" => $torneoId]);
+
+        // Visitante
+        $statsVisitante = $this->getEstadisticas($torneoId, $visitanteId);
+        $dataVisitante  = $this->calcularStats($statsVisitante, $gv, $gl);
+
+        $this->queryBuilder->update($this->table, $dataVisitante, ['equipo_id' => $visitanteId, "torneo_id" => $torneoId]);
+    }
+
+    public function revertirEstadisticas($localId, $visitanteId, $torneoId, $gl, $gv)
+    {
+        // LOCAL
+        $statsLocal = $this->getEstadisticas($torneoId, $localId);
+        $dataLocal  = $this->calcularStatsReversa($statsLocal, $gl, $gv);
+
+        $this->queryBuilder->update(
+            $this->table,
+            $dataLocal,
+            ['equipo_id' => $localId, "torneo_id" => $torneoId]
+        );
+
+        // VISITANTE
+        $statsVisitante = $this->getEstadisticas($torneoId, $visitanteId);
+        $dataVisitante  = $this->calcularStatsReversa($statsVisitante, $gv, $gl);
+
+        $this->queryBuilder->update(
+            $this->table,
+            $dataVisitante,
+            ['equipo_id' => $visitanteId, "torneo_id" => $torneoId]
+        );
+    }
+
+    private function calcularStatsReversa($statsActuales, $gf, $gc)
+    {
+        $pj  = $statsActuales['partidos_jugados'] - 1;
+        $pg  = $statsActuales['ganados'];
+        $pe  = $statsActuales['empatados'];
+        $pp  = $statsActuales['perdidos'];
+        $pts = $statsActuales['puntos'];
+
+        if ($gf > $gc) {
+            $pg  -= 1;
+            $pts -= 3;
+        } elseif ($gf == $gc) {
+            $pe  -= 1;
+            $pts -= 1;
+        } else {
+            $pp -= 1;
+        }
+
+        return [
+            'partidos_jugados' => $pj,
+            'ganados'          => $pg,
+            'empatados'        => $pe,
+            'perdidos'         => $pp,
+            'goles_favor'      => $statsActuales['goles_favor'] - $gf,
+            'goles_contra'     => $statsActuales['goles_contra'] - $gc,
+            'diferencia_goles' => ($statsActuales['goles_favor'] - $gf) -
+            ($statsActuales['goles_contra'] - $gc),
+            'puntos'           => $pts,
+        ];
+    }
+    private function calcularStats($statsActuales, $gf, $gc)
+    {
+        $pj  = $statsActuales['partidos_jugados'] + 1;
+        $pg  = $statsActuales['ganados'];
+        $pe  = $statsActuales['empatados'];
+        $pp  = $statsActuales['perdidos'];
+        $pts = $statsActuales['puntos'];
+
+        if ($gf > $gc) {
+            $pg  += 1;
+            $pts += 3;
+        } elseif ($gf == $gc) {
+            $pe  += 1;
+            $pts += 1;
+        } else {
+            $pp += 1;
+        }
+
+        return [
+            'partidos_jugados' => $pj,
+            'ganados'          => $pg,
+            'empatados'        => $pe,
+            'perdidos'         => $pp,
+            'goles_favor'      => $statsActuales['goles_favor'] + $gf,
+            'goles_contra'     => $statsActuales['goles_contra'] + $gc,
+            'diferencia_goles' => ($statsActuales['goles_favor'] + $gf) - ($statsActuales['goles_contra'] + $gc),
+            'puntos'           => $pts,
+        ];
+    }
+
+    // Obtengo la posicion de un equipo en un torneo
+    public function getPosicion($idTorneo, $idEquipo)
+    {
+        $equipo = $this->queryBuilder->selectViejo(
+            $this->table,
+            [
+                'torneo_id' => $idTorneo,
+                'equipo_id' => $idEquipo,
+            ]
+        )[0];
+
+        $tabla = $this->queryBuilder->selectViejo(
+            $this->table,
+            ['torneo_id' => $idTorneo],
+            'puntos DESC, diferencia_goles DESC'
+        );
+
+        $posicion = 1;
+
+        // Con el equipo y la tabla, busco la posicion buscando puntos y DF
+        foreach ($tabla as $fila) {
+
+            if (
+                $fila['puntos'] > $equipo['puntos'] ||
+                (
+                    $fila['puntos'] == $equipo['puntos'] &&
+                    $fila['diferencia_goles'] > $equipo['diferencia_goles']
+                )
+            ) {
+                $posicion++;
+            }
+        }
+
+        return $posicion;
+    }
+    public function getLastTorneo($idEquipo)
+    {
+        $torneos_disponobles = [];
+        $torneos             = $this->queryBuilder
+            ->select($this->table)
+            ->where("(equipo_id = :id)")
+            ->setParam('id', $idEquipo)
+            ->execute();
+
+        foreach ($torneos as $torneo) {
+            $torneo_id = $torneo['torneo_id'];
+
+            $_torneo = $this->queryBuilder
+                ->select('torneos')
+                ->where("(id = :id AND  estado = 'activo')")
+                ->setParam('id', $torneo_id)
+                ->execute();
+
+            if (! empty($_torneo)) {
+                array_push($torneos_disponobles, $_torneo[0]);
+            }
+        }
+        function primeroCreado(array $torneos)
+        {
+            if (empty($torneos)) {
+                return null;
+            }
+
+            $masViejo = $torneos[0];
+
+            foreach ($torneos as $torneo) {
+                // Comparamos fechas para encontrar el primero
+                if ($torneo['fecha_inicio'] < $masViejo['fecha_inicio']) {
+                    $masViejo = $torneo;
+                }
+            }
+
+            return $masViejo['id'];
+        }
+
+        $torneos_disponobles = primeroCreado($torneos_disponobles);
+
+        return $torneos_disponobles;
+    }
+
+    public function getCantidadEquipos($idTorneo)
+    {
+        return $this->queryBuilder->count($this->table, ['torneo_id' => $idTorneo]);
+    }
 }
